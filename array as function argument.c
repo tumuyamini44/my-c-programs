88d4d056e97f626dd9f7c7ae680268c547be1ed4
tumuyamini44 <tumuyamini44@gmail.com>
2025-11-26 11:11:51 +0530

    Add files via upload

---
+++ array as function argument.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void display(int marks[]);
+main()
+{
+	int marks[]={98,97,89,95,91};
+	display(marks);
+}
+void display(int marks[])
+{
+	int i;
+	printf("the marks are:\n");
+	for(i=0;i<5;i++)
+	{
+		printf("%d\n",marks[i]);
+	}
+}
